a47f1acea80314fbffa9d44773e38a0698eeaf27
alexnaev <102225427+alexnaev@users.noreply.github.com>
2022-04-26 16:26:20 +0200

    Set new main URL

--- app/config/config.php
+++ app/config/config.php
@@ -3,7 +3,7 @@
 define('APPROOT', dirname(dirname(__FILE__)));
 
 //URLROOT (Dynamic links)
-define('URLROOT', 'http://localhost/TrippTrapp');
+define('URLROOT', 'https://tripptrapp.herokuapp.com/');
 
 //Sitename
 define('SITENAME', 'MVC Framework');
